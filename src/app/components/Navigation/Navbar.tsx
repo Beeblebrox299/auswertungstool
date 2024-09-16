2aa1166b4adfb1270b3cc463a21456651b7d1eea
Lena Franke <7franke@informatik.uni-hamburg.de>
2024-09-16 15:07:27 +0200

    add navbar

---
+++ src/app/components/Navigation/Navbar.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import styles from './Navigation.module.css'
+
+const menuItems = [
+    {
+        name: "Startseite",
+        link: "/"
+    },
+    {
+        name: "Bearbeiten",
+        link: "/edit"
+    },
+    {
+        name: "Projektübersicht",
+        link: "/overview"
+    }
+]
+const Sidebar: React.FC = () => {
+    const buildMenu = () => {
+        return menuItems.map((item) => (
+            <Link
+                key={item.name}
+                href={item.link}
+                className='pb-5 md:pl-6'>
+                {item.name}
+            </Link>
+        ));
+    };
+    return(
+        <nav className={styles.menu}>
+            {buildMenu()}
+        </nav>
+    )    
+}
+
+export default Sidebar;
