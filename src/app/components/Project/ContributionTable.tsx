8beb18123e0517d8f16ce7dfcf3b2c7e2792c50b
Lena Franke <7franke@informatik.uni-hamburg.de>
2024-10-07 12:32:12 +0200

    correctly display category

--- src/app/components/Project/ContributionTable.tsx
+++ src/app/components/Project/ContributionTable.tsx
@@ -1,11 +1,26 @@
 import { useEffect, useState } from "react";
-import { getArrayFromStorage } from "@/app/utils";
+import { getCategories, getContributions } from "@/app/utils";
 
 const ContributionTable: React.FC = () => {
     const [contributions, setContributions] = useState<Record<string, any>[]>([])
     useEffect(() => {
-        const contributionArray = getArrayFromStorage("contributions", false)
-        setContributions(contributionArray);
+        const contributionArray = getContributions()
+        const categoryArray = getCategories()
+        const contributionContentArray: Record<string, any>[] = [...contributionArray];
+        contributionContentArray.forEach(contribution => {
+            const getCategoryName = () => {
+                const category = categoryArray.find(category => {
+                    return category.id === contribution.category
+                })
+                if (!category) {
+                    return "Keine Kategorie"
+                }
+                return category.name
+            }
+            delete contribution.id;
+            contribution.category = getCategoryName()
+        });
+        setContributions(contributionContentArray);
     }, [])
     const contributionKeys:string[] = Array.from(new Set(contributions.flatMap(Object.keys)));
 
@@ -16,7 +31,7 @@ const ContributionTable: React.FC = () => {
                     <thead>
                         <tr>
                             {contributionKeys.map((key) => (
-                                <th key={key}>{key}</th>
+                                <th key={key}>{key === "category" ? "Kategorie" : key}</th>
                             ))}
                         </tr>
                     </thead>
