10152cdac4cf80264f329eec46b04064aeda7456
Lena Franke <7franke@informatik.uni-hamburg.de>
2024-10-07 09:31:47 +0200

    display contribution and category selection

--- src/app/components/Project/CodingBox.tsx
+++ src/app/components/Project/CodingBox.tsx
@@ -1,14 +1,26 @@
+import { getArrayFromStorage } from "@/app/utils";
 import React from "react";
 
 const CodingBox: React.FC<{contributionWithId: Record<string, any>, contributionArray:Record<string, any>[]}> = ({contributionWithId, contributionArray}) => {
     const contributionContent = {...contributionWithId};
-    delete contributionContent.id
-    // TODO: Show contribution, add dropdown to choose category, save category selection
+    delete contributionContent.id;
+    const contributionKeys: string[] = Object.keys(contributionContent);
+    const categories = getArrayFromStorage("categories")
+    // TODO: save category selection
     return(
         <div className="displayBlock">
-            <div className="info border">
-                text
-            </div>
+            <span className="info">
+                {contributionKeys.map((key) => (
+                    <div className="info border" key={key}><b>{key}:</b> <br/>{contributionContent[key]}</div>
+                ))}
+                <select defaultValue={"default"} className="info border">
+                    <option disabled value="default">Kategorie auswählen...</option>
+                    {categories.map((category) => (
+                        <option key={category.id} value={category.id}>{category.name}</option>
+                    ))}
+                </select>
+            </span>
+            <button className="btn">Speichern</button>
         </div>
     );
 }
