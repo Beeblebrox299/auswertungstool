b3f72e00c5b648072cd324682c8a17698d22e9f3
Lena Franke <7franke@informatik.uni-hamburg.de>
2024-10-07 14:11:36 +0200

    add component for choosing category field

--- src/app/components/Project/FileUpload.tsx
+++ src/app/components/Project/FileUpload.tsx
@@ -2,11 +2,46 @@ import React, {useState, useRef} from "react";
 import { Contribution, generateId, getContributions } from "@/app/utils";
 import Papa from "papaparse";
 
+interface PopupProps {
+    fields: string[],
+    onSelect: Function,
+};
+
+const CategoryStep: React.FC<PopupProps> = ({fields, onSelect}) => {
+    const [selected, setSelected] = useState<string>("none");
+
+    return (
+        <div>
+            <h2>In welchem Feld befinden sich die Kategorien, die Sie später bearbeiten möchten?</h2>
+            <select defaultValue={"none"} onChange={(e) => setSelected(e.target.value)} className="info">
+                <option value={"none"}>
+                    Keins
+                </option>
+                {fields.map((field) => (
+                    <option key={field} value={field}>
+                        {field}
+                    </option>
+                ))}
+            </select>
+            <button className="btn" onClick={() => onSelect(selected)}>OK</button>
+        </div>
+    )
+};
+
 const FileUpload: React.FC = () => {
     const fileInputRef = useRef<HTMLInputElement>(null)
 
+    // useStates for conditional rendering
+    const [fileUploaded, setFileUploaded] = useState<boolean>(false);
+    const [categoryFieldSet, setCategoryFieldSet] = useState<boolean>(false);
+
+    const [fields, setFields] = useState<string[]>([]);
+
     // File uploaded by user
     const [file, setFile] = useState<File|null>(null);
+
+    // parsed file
+    const [data, setData] = useState<Record<string, any>|null>(null)
 
     // Message to show after user hits 'submit'
     const [messageText, setMessageText] = useState<string>('');
@@ -26,23 +61,9 @@ const FileUpload: React.FC = () => {
                 header: true,
                 skipEmptyLines: true,
                 complete: function (results: any) {
-                    const newFields: string[] = results.meta.fields;
-                    // TODO: Ask user, which field (if any) holds the categorisation and whether to merge any column with an existing field -> Convert categories from string to references to category objects and update project fields
-                    const storedFieldsString = localStorage.getItem("fields");
-                    const storedFields:string[] = (storedFieldsString != null) ? JSON.parse(storedFieldsString) : [];
-                    const fields = Array.from(new Set([...storedFields, ...newFields]));
-                    localStorage.setItem("fields", JSON.stringify(fields))
-                    const newContributions:Contribution[] = results.data
-                    const storedContributions = getContributions();
-                    newContributions.forEach(contribution => {
-                        contribution.id = generateId();
-                    });
-                    const contributions = [...storedContributions, ...newContributions];
-                    localStorage.setItem("contributions", JSON.stringify(contributions));
-                    if (fileInputRef.current) {
-                        fileInputRef.current.value = ""
-                    }
-                    setMessageText('"' + file.name + '" wurde hochgeladen');
+                    setFields(results.meta.fields);
+                    setData(results.data);
+                    setFileUploaded(true);
                 },
               });
         }
@@ -51,17 +72,54 @@ const FileUpload: React.FC = () => {
             setMessageText("Bitte eine Datei auswählen!");
         };
         return
+    };
+
+    const handleSelect = (selectedField: string) => {
+        /* 
+        TODO: 
+        - convert values of selectedField names to category-IDs
+        - set "category" property
+
+        Bigger Todos:
+        - Enable Multi-Category contributions
+            -> Ask user for seperator in file values
+
+        copied from handleSubmit:
+        localStorage.setItem("fields", JSON.stringify(fields))
+        const newContributions:Contribution[] = results.data
+        const storedContributions = getContributions();
+        newContributions.forEach(contribution => {
+            contribution.id = generateId();
+        });
+        const contributions = [...storedContributions, ...newContributions];
+        localStorage.setItem("contributions", JSON.stringify(contributions));
+        if (fileInputRef.current) {
+            fileInputRef.current.value = ""
+        }
+        setMessageText('"' + file.name + '" wurde hochgeladen'); */
+        setCategoryFieldSet(true)
     }
 
     return(
         <div>
-        <form onSubmit={handleSubmit}>
-            <input type="file" accept=".csv" ref={fileInputRef} onChange={e => handleChange(e.target.files)}/>
-            <br/>
-            <button type="submit" className="btn">Hochladen</button>
-            <br/>
-            {messageText}
-        </form>
+        {(!fileUploaded && !categoryFieldSet) && (
+            <form onSubmit={handleSubmit}>
+                <input type="file" accept=".csv" ref={fileInputRef} onChange={e => handleChange(e.target.files)}/>
+                <br/>
+                <button type="submit" className="btn">Hochladen</button>
+                <br/>
+            </form>
+        )}
+        {(fileUploaded && !categoryFieldSet) && (
+            <CategoryStep 
+                fields={fields}
+                onSelect={handleSelect}
+            />
+        )}
+        {messageText}
+        {(fileUploaded && categoryFieldSet) && (
+            <button>Weitere Datei hochladen</button>
+        )}
         </div>
     );
 }
