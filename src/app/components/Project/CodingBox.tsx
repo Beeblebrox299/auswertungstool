cc199200bce951f4718f5003e25ed3b87d157cad
Lena Franke <7franke@informatik.uni-hamburg.de>
2024-10-07 12:31:59 +0200

    save category selection

--- src/app/components/Project/CodingBox.tsx
+++ src/app/components/Project/CodingBox.tsx
@@ -1,26 +1,58 @@
-import { getArrayFromStorage } from "@/app/utils";
-import React from "react";
+import { getCategories, Category, Contribution } from "@/app/utils";
+import React, { useState } from "react";
 
-const CodingBox: React.FC<{contributionWithId: Record<string, any>, contributionArray:Record<string, any>[]}> = ({contributionWithId, contributionArray}) => {
-    const contributionContent = {...contributionWithId};
+const CodingBox: React.FC<{contributionWithId: Contribution, contributionArray: Contribution[]}> = ({contributionWithId, contributionArray}) => {
+    const [categoryId, setCategoryId] = useState<string|null>(null)
+    
+    const contributionContent: Record<string, any> = {...contributionWithId};
     delete contributionContent.id;
+    delete contributionContent.category;
+
     const contributionKeys: string[] = Object.keys(contributionContent);
-    const categories = getArrayFromStorage("categories")
-    // TODO: save category selection
+    const categories: Category[] = getCategories();
+
+    const setDefault = () => {
+        if (!contributionContent.category) {
+            return "default"
+        }
+        return contributionContent.category
+    };
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        if (!categoryId) {
+            throw new Error("CategoryId empty")
+        }
+        categories.forEach(category => {
+            if (category.id === JSON.parse(categoryId)) {
+                category.assignedTo.push(contributionWithId.id)
+            }
+        });
+        contributionArray.forEach(contribution => {
+            if (contribution.id === contributionWithId.id) {
+                contribution.category = JSON.parse(categoryId)
+            }
+        });
+        localStorage.setItem("contributions", JSON.stringify(contributionArray))
+        localStorage.setItem("categories", JSON.stringify(categories))
+    };
+    
     return(
         <div className="displayBlock">
             <span className="info">
                 {contributionKeys.map((key) => (
                     <div className="info border" key={key}><b>{key}:</b> <br/>{contributionContent[key]}</div>
                 ))}
-                <select defaultValue={"default"} className="info border">
-                    <option disabled value="default">Kategorie auswählen...</option>
-                    {categories.map((category) => (
-                        <option key={category.id} value={category.id}>{category.name}</option>
-                    ))}
-                </select>
+                <form onSubmit={e => handleSubmit(e)}>
+                    <select id="categorySelect" defaultValue={setDefault()} className="info border" onChange={e => setCategoryId(e.target.value)}>
+                        <option disabled value="default">Kategorie auswählen...</option>
+                        {categories.map((category) => (
+                            <option key={category.id} value={category.id}>{category.name}</option>
+                        ))}
+                    </select>
+                    <button className="btn" type="submit">Speichern</button>
+                </form>
             </span>
-            <button className="btn">Speichern</button>
         </div>
     );
 }
