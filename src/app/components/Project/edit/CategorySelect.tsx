1775f6e377af428c0c84105ebb8859c81cf13ae8
Lena Franke <7franke@informatik.uni-hamburg.de>
2024-10-23 14:35:53 +0200

    Add ability to create new category at coding stage

--- src/app/components/Project/edit/CategorySelect.tsx
+++ src/app/components/Project/edit/CategorySelect.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Category } from "@/app/utils";
+import { Category, generateId } from "@/app/utils";
 import { FaMinusCircle, FaPlusCircle } from "react-icons/fa";
 
 const CategorySelect: React.FC<{onCategorySelect(categoryIds?: number[]): void, initialCategoryIds: number[], categories: Category[]}> = ({onCategorySelect, initialCategoryIds, categories}) => {
@@ -16,8 +16,23 @@ const CategorySelect: React.FC<{onCategorySelect(categoryIds?: number[]): void, 
 
     const handleCategoryChange = (target: EventTarget & HTMLSelectElement) => {
         const index = parseInt(target.id);
-        const newCategoryIds = categoryIds;
-        newCategoryIds[index] = parseInt(target.value);
+        const newCategoryIds = [...categoryIds];
+        if (target.value === "new") {
+            const newCategoryName = window.prompt("Geben Sie den Namen der neuen Kategorie ein", "Neue Kategorie")
+            if (newCategoryName === null) return
+            if (newCategoryName === "") throw new Error("category name cannot be empty")
+            const newCategory: Category = {
+                id: generateId(),
+                name: newCategoryName,
+                assignedTo: [],
+            };
+            categories.push(newCategory);
+            if (typeof window !== "undefined") {
+                localStorage.setItem("categories", JSON.stringify(categories))
+            };
+            newCategoryIds[index] = newCategory.id;
+        }
+        else newCategoryIds[index] = parseInt(target.value);
         onCategorySelect(newCategoryIds);
     };
 
@@ -43,7 +58,7 @@ const CategorySelect: React.FC<{onCategorySelect(categoryIds?: number[]): void, 
             {getRemainingCategories(index).map((category) => (
                 <option key={category.id} value={category.id.toString()}>{category.name}</option>
             ))}
-            <option value="new">Neue Kategorie erstellen</option> {/* TODO: Implement function to add new category*/}
+            <option value="new">Neue Kategorie erstellen</option>
         </select>
         {(index > 0) ? <button type="button" className="btn" onClick={() => {
                 const updatedCategoryIds = categoryIds.filter((_, idx) => idx != index);
