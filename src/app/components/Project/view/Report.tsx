1893b3142e2e8773fc7a9489734237f5640a67c6
Lena Franke <7franke@informatik.uni-hamburg.de>
2024-10-28 11:15:44 +0100

    Add report form

--- src/app/components/Project/view/Report.tsx
+++ src/app/components/Project/view/Report.tsx
@@ -1,9 +1,24 @@
 import {Document, Page, Image, Text} from "@react-pdf/renderer";
 
-const Report: React.FC = () => {
+const renderReport = () => {
+
+}
+
+const ReportForm: React.FC = () => {
     return (
         <div className="displayBlock">
-            
+            <form>
+                <div className="info border">
+                    <h2>Titel des Berichts: </h2>
+                    <input type="text" id="title" defaultValue={"Titel"}></input>
+                </div><br/>
+                <div className="info border">
+                    <h2>Einleitung: </h2>
+                    <textarea cols={50} rows={8}/>
+                </div>
+            </form>
         </div>
     )
-}
+};
+
+export default ReportForm;
---
+++ src/app/report/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+import ReportForm from '../components/Project/view/Report';
+
+const Report: React.FC = () => {
+    return (
+        <div>
+            <ReportForm/>
+        </div>
+    );
+}
+
+export default Report;
