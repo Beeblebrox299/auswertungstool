cad3c2db9ab744c12a1975f0a8605350b9404885
Lena Franke <7franke@informatik.uni-hamburg.de>
2024-10-28 08:44:09 +0100

    report page

---
+++ src/app/components/Project/view/Report.tsx
@@ -0,0 +1,9 @@
+import {Document, Page, Image, Text} from "@react-pdf/renderer";
+
+const Report: React.FC = () => {
+    return (
+        <div className="displayBlock">
+            
+        </div>
+    )
+}
