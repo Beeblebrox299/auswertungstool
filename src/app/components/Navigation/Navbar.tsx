e2af2507343d7b537bc6fe06b2887f681a8369fb
Lena Franke <7franke@informatik.uni-hamburg.de>
2024-10-28 11:15:32 +0100

    Adjust Navbar

--- src/app/components/Navigation/Navbar.tsx
+++ src/app/components/Navigation/Navbar.tsx
@@ -1,34 +1,79 @@
 import Link from "next/link";
-import styles from './Navigation.module.css'
 
-const menuItems = [
+interface MenuItem {
+    name: string,
+    link: string,
+    submenu?: MenuItem[]
+}
+
+const menuItems:MenuItem[] = [
     {
         name: "Startseite",
         link: "/"
     },
     {
-        name: "Bearbeiten",
-        link: "/edit"
+        name: "Beiträge hinzufügen",
+        link: "",
+        submenu: [
+            {name: "Datei hochladen", link: "/edit/file-upload"},
+            {name: "Manuell eintragen", link: "/edit/manual"}
+        ]
     },
     {
-        name: "Projektübersicht",
-        link: "/overview"
+        name: "Codierung",
+        link: "/edit/coding"
+    },
+    {
+        name: "Auswertung",
+        link: "",
+        submenu: [
+            {name: "Projektübersicht", link: "/overview"},
+            {name: "Grafiken / Diagramme", link: "/analysis"},
+            {name: "Bericht", link: "/report"}
+        ]
+    },
+    {
+        name: "Einstellungen",
+        link: "/edit/categories"
     }
 ]
 const Navbar: React.FC = () => {
-    const buildMenu = () => {
+    const buildMenu = (menuItems: MenuItem[]) => {
+        const buildMenuItem = (item: MenuItem) => {
+            return(
+                <div key={item.name} className="relative group">
+                    {(item.link !== "") ?
+                    <Link
+                        href={item.link}
+                        className='block p-5 hover:text-gray-600 md:pl-6 text-lg'>
+                        {item.name}
+                    </Link> :
+                    <span
+                        className='block p-5 md:pl-6 text-lg'>
+                        {item.name}
+                    </span>}
+                    {item.submenu && (
+                        <div className="absolute hidden bg-gray-200 mt-0 left-full top-0 p-2 group-hover:block w-52">
+                            {item.submenu.map((subitem => (
+                                <Link
+                                    key={subitem.name}
+                                    href={subitem.link}
+                                    className="block px-4 py-2 text-black hover:bg-gray-300"
+                                >
+                                    {subitem.name}
+                                </Link>
+                            )))}
+                        </div>
+                    )}
+                </div>)
+        }
         return menuItems.map((item) => (
-            <Link
-                key={item.name}
-                href={item.link}
-                className='pb-5 md:pl-6'>
-                {item.name}
-            </Link>
+            buildMenuItem(item)
         ));
     };
     return(
-        <nav className={styles.menu}>
-            {buildMenu()}
+        <nav className="bg-gray-300 p-5 w-52 h-screen fixed">
+            {buildMenu(menuItems)}
         </nav>
     )    
 }
