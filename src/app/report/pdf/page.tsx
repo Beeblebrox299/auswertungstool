7cfd0d0556518fd0130024e479aacfe78a1bafe8
Lena Franke <7franke@informatik.uni-hamburg.de>
2024-10-29 08:03:32 +0100

    downgrade react-pdf and adjust PDFViewer import

--- src/app/report/pdf/page.tsx
+++ src/app/report/pdf/page.tsx
@@ -1,7 +1,17 @@
 'use client';
 
 import { Image } from '@/app/utils';
+import { Document, Page, Text, View, Image as PDFImage } from '@react-pdf/renderer';
 import React, { useEffect, useState } from 'react';
+import dynamic from "next/dynamic";
+
+const PDFViewer = dynamic(
+  () => import("@react-pdf/renderer").then((mod) => mod.PDFViewer),
+  {
+    ssr: false,
+    loading: () => <p>PDF wird erstellt...</p>,
+  },
+);
 
 const Report: React.FC = () => {
     const [reportContent, setReportContent] = useState<{titel: string,einleitung: string,images: Image[]}>({titel: "", einleitung: "", images: []});
@@ -17,8 +27,28 @@ const Report: React.FC = () => {
     }, [])
 
     return (
-        <div className='displayBlock'>
-            Erstelle PDF...
+        <div className='h-screen w-screen'>
+            <PDFViewer className='pl-52 h-full w-full'>
+                <Document>
+                    <Page size="A4">
+                        <View style={{ color: 'black', textAlign: 'center', margin: 30, fontSize: 30 }}>
+                            <Text>{reportContent.titel}</Text>
+                        </View>
+                        <View style={{fontSize: 12, margin: 10}}>
+                            <Text>{reportContent.einleitung + "\n \n"}</Text>
+                            {images.map((image, index) => (
+                                <View key={index}>
+                                <Text>{image.description + "\n\n"} 
+                                    
+                                </Text>
+                                <Text>Abbildung : {image.name}</Text>
+                                <PDFImage style={{}} src={image.src}/>
+                                </View>
+                            ))}
+                        </View>
+                    </Page>
+                </Document>
+            </PDFViewer>
         </div>
     );
 }
